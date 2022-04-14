c83f29353a850e4f98fc4f2a7cdc59e102dd2c66
Fernando Gomes <feromes@gmail.com>
2022-04-14 17:02:56 -0300

    Heroku Deploy

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 # Run this app with `python app.py` and
 # visit http://127.0.0.1:8050/ in your web browser.
 
-from cgitb import enable
-from faulthandler import disable
+# from cgitb import enable
+# from faulthandler import disable
 import geopandas as gpd
 import pandas as pd
 from dash import Dash, html, dcc, Input, Output, dash_table
