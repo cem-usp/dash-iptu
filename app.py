a96f08132bbc602f1a77ef4c28fdce943c4d3465
Fernando Gomes <feromes@gmail.com>
2022-04-28 16:28:38 -0300

    Adicionando novas funcionalidades e agregacoes espaciais

--- app.py
+++ app.py
@@ -5,17 +5,32 @@
 # from faulthandler import disable
 import geopandas as gpd
 import pandas as pd
-from dash import Dash, html, dcc, Input, Output, dash_table
+from dash import Dash, html, dcc, Input, Output, State, dash_table
 import dash
 import dash_bootstrap_components as dbc
 import plotly.express as px
 import vaex
 
-df_iptu_distritos = vaex.open('data/IPTU-Distritos1995-2022-agrupados-por-distrito.hdf5')
+df_iptu_distrito = vaex.open('data/IPTU-1995-2022-agrupados-por-distrito.hdf5')
+df_iptu_subprefeitura = vaex.open('data/IPTU-1995-2022-agrupados-por-subprefeitura.hdf5')
+df_iptu_od = vaex.open('data/IPTU-1995-2022-agrupados-por-od.hdf5')
+
 gdf_distritos = gpd.read_file('data/SIRGAS_GPKG_distrito.gpkg')
 gdf_distritos['area'] = gdf_distritos.area
+gdf_distritos.geometry = gdf_distritos.simplify(tolerance=100)
 gdf_distritos.to_crs(epsg=4674, inplace=True)
-# df_iptu_distritos = df_iptu_distritos.merge(gdf_distritos.loc[:, ['ds_codigo', 'ds_nome']].astype({'ds_codigo': 'int'}), left_on='distrito', right_on='ds_codigo')
+
+gdf_od = gpd.read_file('zip://data/SIRGAS_SHP_origemdestino_2017.zip!SIRGAS_SHP_origemdestino_2017', crs='EPSG:31983')
+gdf_od = gdf_od[gdf_od.od_municip == '36']
+gdf_od.set_crs(epsg=31983, inplace=True)
+gdf_od['area'] = gdf_od.area
+gdf_od.geometry = gdf_od.simplify(tolerance=10)
+gdf_od.to_crs(epsg=4674, inplace=True)
+
+gdf_subprefeitura = gpd.read_file('zip://data/SIRGAS_GPKG_subprefeitura.zip!SIRGAS_GPKG_subprefeitura.gpkg')
+gdf_subprefeitura['area'] = gdf_subprefeitura.area
+gdf_subprefeitura.geometry = gdf_subprefeitura.simplify(tolerance=100)
+gdf_subprefeitura.to_crs(epsg=4674, inplace=True)
 
 app = dash.Dash(__name__,
     external_stylesheets=[dbc.themes.BOOTSTRAP]
@@ -30,9 +45,9 @@ radioitems = html.Div(
         dbc.Label("Selecione a agregação espacial"),
         dbc.RadioItems(
             options=[
+                {"label": "Subprefeituras", "value": 2, "disabled": False},
                 {"label": "Distritos", "value": 1},
-                {"label": "Subprefeituras", "value": 2, "disabled": True},
-                {"label": "Zonas OD", "value": 3, "disabled": True},
+                {"label": "Zonas OD", "value": 3, "disabled": False},
                 {"label": "CODLOGs", "value": 4, "disabled": True},
             ],
             value=1,
@@ -50,11 +65,11 @@ checklist = html.Div(
                 {"label": "Quantidade de Unidades Condominiais", "value": "Quantidade de Unidades Condominiais"},
                 {"label": "Tamanho Médio da Unidade Condominial", "value": "Tamanho Médio da Unidade Condominial"},
                 {"label": "Tamanho médio dos Terrenos", "value": "Tamanho médio dos Terrenos"},
-                {"label": "Área Total dos terrenos/lotes", "value": "Área Total dos terrenos/lotes", "disabled": True},
-                {"label": "Área Total Ocupada", "value": "Área Total Ocupada", "disabled": True},
+                {"label": "Área Total dos terrenos-lotes", "value": "Área Total dos terrenos-lotes", "disabled": False},
+                {"label": "Área Total Ocupada", "value": "Área Total Ocupada", "disabled": False},
                 {"label": "Área Total Construída", "value": "Área Total Construída"},
-                {"label": "Valor Total dos Terrenos", "value": "Valor Total dos Terrenos", "disabled": True},
-                {"label": "Valor Total das Construções", "value": "Valor Total das Construções", "disabled": True},
+                {"label": "Valor Total dos Terrenos", "value": "Valor Total dos Terrenos", "disabled": False},
+                {"label": "Valor Total das Construções", "value": "Valor Total das Construções", "disabled": False},
                 {"label": "CA médio", "value": "CA médio"},
                 {"label": "TO médio", "value": "TO médio"},
                 {"label": "CA médio em lotes condominiais", "value": "CA médio em lotes condominiais"},
@@ -132,21 +147,49 @@ app.layout = dbc.Container(
 @app.callback(
     Output("graph", "figure"), 
     Input("checklist-input", "value"),
-    Input("range-slider", "value"))
-def update_map(atributo, ano):
-    gdf_distritos_counted = gdf_distritos.astype({'ds_codigo': 'int'}).merge(df_iptu_distritos[df_iptu_distritos.ano == ano].to_pandas_df(), left_on='ds_codigo', right_on='distrito')
-    fig = px.choropleth(gdf_distritos_counted,
-                    geojson=gdf_distritos_counted.geometry,
-                    projection="transverse mercator",
+    Input("range-slider", "value"),
+    Input("radioitems-input", "value"),
+    State('graph', 'figure'))
+def update_map(atributo, ano, agregacao, mapa_atual):
+
+    if mapa_atual:
+        zoom = mapa_atual['layout']['mapbox']['zoom']
+        center = mapa_atual['layout']['mapbox']['center']
+    else:
+        zoom, center = 9, {'lat':-23.62095411, 'lon':-46.61666592}
+
+    # if agregacao == 1:
+    #     gdf_agregacao = gdf_distritos.astype({'ds_codigo': 'int'}).merge(df_iptu_distrito[df_iptu_distrito.ano == ano].to_pandas_df(), left_on='ds_codigo', right_on='distrito')
+    #     hover_data = ["ds_nome"]
+    #     custom_data=["ds_codigo"]
+
+    # if agregacao == 2:
+    #     gdf_agregacao = gdf_subprefeitura.astype({'sp_codigo': 'int'}).merge(df_iptu_subprefeitura[df_iptu_subprefeitura.ano == ano].to_pandas_df(), left_on='sp_codigo', right_on='subprefeitura')
+    #     hover_data = ["sp_nome"]
+    #     custom_data=["sp_codigo"]
+
+    # if agregacao == 3:
+    #     gdf_agregacao = gdf_od.astype({'od_id': 'int'}).merge(df_iptu_od[df_iptu_od.ano == ano].to_pandas_df(), left_on='od_id', right_on='od')        
+    #     hover_data = ["od_nome"]
+    #     custom_data=["od_id"]
+
+    gdf_agregacao, hover_data, custom_data = sel_agregacao(agregacao, ano)
+
+    fig = px.choropleth_mapbox(gdf_agregacao,
+                    geojson=gdf_agregacao.geometry,
+                    zoom=zoom,
+                    center=center,
+                    #  projection="transverse mercator",
                     color=atributo,
-                    locations=gdf_distritos_counted.index.to_list(),
-                    hover_data=["ds_nome"],
-                    custom_data=["ds_codigo"],
-                    fitbounds='geojson',
-                    title=f'{atributo} para o ano de {ano[-1]}')
-                    #  color="ds_codigo")
-                    #    projection="mercator")
-    fig.update_geos(fitbounds="locations", visible=False)
+                    locations=gdf_agregacao.index.to_list(),
+                    mapbox_style="white-bg",
+                    hover_data=hover_data,
+                    custom_data=custom_data,
+                    height=600,
+                    width=800)
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0},
+                    title={'text': 'Seila'})
+
     return fig
 
 @app.callback(
@@ -154,7 +197,7 @@ def update_map(atributo, ano):
     Output("table", "data"), 
     Input("checklist-input", "value"))
 def gen_table(atributo):
-    df = df_iptu_distritos.to_pandas_df().merge(gdf_distritos.loc[:, ['ds_codigo', 'ds_nome']].astype({'ds_codigo': 'int'}), left_on='distrito', right_on='ds_codigo')
+    df = df_iptu_distrito.to_pandas_df().merge(gdf_distritos.loc[:, ['ds_codigo', 'ds_nome']].astype({'ds_codigo': 'int'}), left_on='distrito', right_on='ds_codigo')
     df_pivot = df.pivot(index=['distrito', 'ds_nome'], values=atributo, columns='ano')
     columns=[{"name": str(i), "id": str(i)} for i in df_pivot.reset_index().columns]
     data=df_pivot.reset_index().to_dict("records")
@@ -164,7 +207,26 @@ def gen_table(atributo):
     Output("table-title", "children"),
     Input("checklist-input", "value"))
 def change_title(atributo):
-    return f'Download da série histórica para {atributo}'
+    return f'Download da série histórica para {atributo} por Distrito'
+
+def sel_agregacao(agregacao, ano):
+
+    if agregacao == 1:
+        gdf_agregacao = gdf_distritos.astype({'ds_codigo': 'int'}).merge(df_iptu_distrito[df_iptu_distrito.ano == ano].to_pandas_df(), left_on='ds_codigo', right_on='distrito')
+        hover_data = ["ds_nome"]
+        custom_data=["ds_codigo"]
+
+    if agregacao == 2:
+        gdf_agregacao = gdf_subprefeitura.astype({'sp_codigo': 'int'}).merge(df_iptu_subprefeitura[df_iptu_subprefeitura.ano == ano].to_pandas_df(), left_on='sp_codigo', right_on='subprefeitura')
+        hover_data = ["sp_nome"]
+        custom_data=["sp_codigo"]
+
+    if agregacao == 3:
+        gdf_agregacao = gdf_od.astype({'od_id': 'int'}).merge(df_iptu_od[df_iptu_od.ano == ano].to_pandas_df(), left_on='od_id', right_on='od')        
+        hover_data = ["od_nome"]
+        custom_data=["od_id"]
+
+    return gdf_agregacao, hover_data, custom_data
 
 if __name__ == '__main__':
     # app.run_server(debug=True)
