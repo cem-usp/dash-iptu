9e10fa82603de9ed59324ea111092308368e2844
Fernando Gomes <feromes@gmail.com>
2022-04-29 11:32:57 -0300

    resolvendo proxypass

--- app.py
+++ app.py
@@ -33,13 +33,17 @@ gdf_subprefeitura.geometry = gdf_subprefeitura.simplify(tolerance=100)
 gdf_subprefeitura.to_crs(epsg=4674, inplace=True)
 
 app = dash.Dash(__name__,
-    external_stylesheets=[dbc.themes.BOOTSTRAP],
-    routes_pathname_prefix='/dash-iptu/',
+    external_stylesheets=[dbc.themes.BOOTSTRAP]
+)
+
+app.config.update({
+    # as the proxy server will remove the prefix
+    'routes_pathname_prefix': '/', 
 
     # the front-end will prefix this string to the requests
     # that are made to the proxy server
-    requests_pathname_prefix='/dash-iptu/'
-)
+    'requests_pathname_prefix': '/dash-iptu/'
+})
 
 server = app.server
 
