bb4a2d1fc710b4f9673721c2dcc83309c94cea67
Fernando Gomes <feromes@gmail.com>
2022-05-25 16:26:48 -0300

    Versao 0.4

--- app.py
+++ app.py
@@ -3,6 +3,7 @@
 
 # from cgitb import enable
 # from faulthandler import disable
+from certifi import contents
 import geopandas as gpd
 import pandas as pd
 from dash import Dash, html, dcc, Input, Output, State, dash_table
@@ -15,6 +16,7 @@ import os
 df_iptu_distrito = vaex.open('data/IPTU-1995-2022-agrupados-por-distrito.hdf5')
 df_iptu_subprefeitura = vaex.open('data/IPTU-1995-2022-agrupados-por-subprefeitura.hdf5')
 df_iptu_od = vaex.open('data/IPTU-1995-2022-agrupados-por-od.hdf5')
+df_iptu_sq = vaex.open('data/IPTU-1995-2022-agrupados-por-sq.hdf5')
 
 gdf_distritos = gpd.read_file('data/SIRGAS_GPKG_distrito.gpkg')
 gdf_distritos['area'] = gdf_distritos.area
@@ -32,6 +34,9 @@ gdf_subprefeitura = gpd.read_file('zip://data/SIRGAS_GPKG_subprefeitura.zip!SIRG
 gdf_subprefeitura['area'] = gdf_subprefeitura.area
 gdf_subprefeitura.geometry = gdf_subprefeitura.simplify(tolerance=100)
 gdf_subprefeitura.to_crs(epsg=4674, inplace=True)
+
+# gdf_quadras = gpd.read_file('data/quadras.gpkg')
+# gdf_quadras.to_crs(epsg=4674, inplace=True)
 
 routes_pathname_prefix='/dash-iptu/'
 
@@ -60,12 +65,19 @@ radioitems = html.Div(
         dbc.Label("Selecione a agregação espacial"),
         dbc.RadioItems(
             options=[
-                {"label": "Subprefeituras", "value": 2, "disabled": False},
-                {"label": "Distritos", "value": 1},
-                {"label": "Zonas OD", "value": 3, "disabled": False},
-                {"label": "CODLOGs", "value": 4, "disabled": True},
+                # {"label": "Subprefeituras", "value": 2, "disabled": False},
+                # {"label": "Distritos", "value": 1},
+                # {"label": "Zonas OD", "value": 3, "disabled": False},
+                # {"label": "Macroáreas PDE", "value": 4, "disabled": True},
+                # {"label": "Quadras", "value": 5, "disabled": True},
+                {"label": "Subprefeituras", "value": 'subprefeituras', "disabled": False},
+                {"label": "Distritos", "value": 'distritos'},
+                {"label": "Zonas OD", "value": 'zonas-od', "disabled": False},
+                {"label": "Macroáreas PDE", "value": 'macro-areas', "disabled": True},
+                {"label": "Quadras", "value": 'quadras', "disabled": True},
+                
             ],
-            value=1,
+            value='distritos',
             id="radioitems-input"
         ),
     ]
@@ -76,7 +88,7 @@ checklist = html.Div(
         dbc.Label("Selecione as totalizações, índices ou Quantitativos"),
         dbc.RadioItems(
             options=[
-                {"label": "Quantidade de Unidades", "value": "Quantidade de Unidades", "disabled": False},
+                # {"label": "Quantidade de Unidades", "value": "Quantidade de Unidades", "disabled": False},
                 {"label": "Quantidade de Unidades Condominiais", "value": "Quantidade de Unidades Condominiais"},
                 {"label": "Tamanho Médio da Unidade Condominial", "value": "Tamanho Médio da Unidade Condominial"},
                 {"label": "Tamanho médio dos Terrenos", "value": "Tamanho médio dos Terrenos"},
@@ -108,16 +120,16 @@ checklist = html.Div(
 
 range_slider = html.Div(
     [
-        dbc.Label("Exercício", html_for="range-slider"),
+        dbc.Label("82.322.059 registros calculados", id="registros-calculados"),
         dcc.RangeSlider(id="range-slider", 
                         min=1995, 
                         max=2022, 
                         step=1, 
                         updatemode='drag',
                         # value=[2022,2022],
-                        value=[2022],
+                        value=[1995,2022],
                         marks={1995: '1995',2000: '2000', 2005: '2005', 2010: '2010', 2015: '2015', 2020: '2020'},
-                        pushable=False),
+                        pushable=True),
     ],
     className="ano-exercicio",
 )
@@ -133,6 +145,11 @@ app.layout = dbc.Container(
 
                 Código disponível em [https://github.com/cem-usp/dash-iptu] comentários, sugestões, inconsistências reportar preferencialmente por `issue` no GitHub ou por email para [feromes@usp.br](mailto:feromes@usp.br)
                 '''),
+                dcc.Tabs(id='tabs-example-1', value='tab-1', children=[
+                    dcc.Tab(label='Área Total Construída em 2022', value='tab-1', id='tab-1'),
+                    dcc.Tab(label='Variação de 1995 à 2022', value='tab-2', id='tab-2'),
+                    dcc.Tab(label='Descrição do cálculo/processamento para Área Total Construída', value='tab-3', id='tab-3'),
+                ]),
                 dcc.Loading(
                     id='loading-map',
                     type='default',
@@ -150,18 +167,20 @@ app.layout = dbc.Container(
                 html.Br()]
             ),
             dbc.Col(
-                dcc.Graph(id="graph"), width=8
+                [dcc.Graph(id="graph"),
+                dbc.Button('Download', id='download-button', n_clicks=0),
+                dcc.Download(id="download-gpkg")], width=8
             )]
         ),
-        dbc.Row(
-            dbc.Col(
-                [html.H2('Download da série histórica para ...', id="table-title"),
-                dash_table.DataTable(
-                    id='table',
-                    export_format="csv"
-                )]
-            )
-        )
+        # dbc.Row(
+        #     dbc.Col(
+        #         [html.H2('Download da série histórica para ...', id="table-title"),
+        #         dash_table.DataTable(
+        #             id='table',
+        #             export_format="csv"
+        #         )]
+        #     )
+        # )
     ])
 
 # @app.callback(
@@ -175,6 +194,10 @@ app.layout = dbc.Container(
 @app.callback(
     Output("graph", "figure"), 
     Output("loading-map", "children"),
+    Output("registros-calculados", 'children'),
+    Output("tab-1", "label"),
+    Output("tab-2", "label"),
+    Output("tab-3", "label"),
     Input("checklist-input", "value"),
     Input("range-slider", "value"),
     Input("radioitems-input", "value"),
@@ -202,7 +225,7 @@ def update_map(atributo, ano, agregacao, mapa_atual):
     #     hover_data = ["od_nome"]
     #     custom_data=["od_id"]
 
-    gdf_agregacao, hover_data, custom_data, min_max = sel_agregacao(agregacao, ano, atributo)
+    gdf_agregacao, hover_data, custom_data, min_max = sel_agregacao(agregacao, ano[-1], atributo)
 
     fig = px.choropleth_mapbox(gdf_agregacao,
                     geojson=gdf_agregacao.geometry,
@@ -223,47 +246,72 @@ def update_map(atributo, ano, agregacao, mapa_atual):
 
     loading = None
 
-    return fig, loading
+    registros = f"{format(gdf_agregacao['Quantidade de Unidades'].sum(), ',d').replace(',', '.')} registros calculados"
+    tab1 = f"{atributo} em {ano[-1]}"
+    tab2 = f"Variação em {atributo} de {ano[0]} à {ano[-1]}"
+    tab3 = f"Descrição do cálculo/processamento para {atributo}"
 
-@app.callback(
-    Output("table", "columns"),
-    Output("table", "data"), 
-    Input("checklist-input", "value"))
-def gen_table(atributo):
-    df = df_iptu_distrito.to_pandas_df().merge(gdf_distritos.loc[:, ['ds_codigo', 'ds_nome']].astype({'ds_codigo': 'int'}), left_on='distrito', right_on='ds_codigo')
-    df_pivot = df.pivot(index=['distrito', 'ds_nome'], values=atributo, columns='ano')
-    columns=[{"name": str(i), "id": str(i)} for i in df_pivot.reset_index().columns]
-    data=df_pivot.reset_index().to_dict("records")
-    return columns, data
+    return fig, loading, registros, tab1, tab2, tab3 
 
-@app.callback(
-    Output("table-title", "children"),
-    Input("checklist-input", "value"))
-def change_title(atributo):
-    return f'Download da série histórica para {atributo} por Distrito'
+# @app.callback(
+#     Output("table", "columns"),
+#     Output("table", "data"), 
+#     Input("checklist-input", "value"))
+# def gen_table(atributo):
+#     df = df_iptu_distrito.to_pandas_df().merge(gdf_distritos.loc[:, ['ds_codigo', 'ds_nome']].astype({'ds_codigo': 'int'}), left_on='distrito', right_on='ds_codigo')
+#     df_pivot = df.pivot(index=['distrito', 'ds_nome'], values=atributo, columns='ano')
+#     columns=[{"name": str(i), "id": str(i)} for i in df_pivot.reset_index().columns]
+#     data=df_pivot.reset_index().to_dict("records")
+#     return columns, data
+
+# @app.callback(
+#     Output("table-title", "children"),
+#     Input("checklist-input", "value"))
+# def change_title(atributo):
+#     return f'Download da série histórica para {atributo} por Distrito'
 
 def sel_agregacao(agregacao, ano, atributo):
 
-    if agregacao == 1:
-        gdf_agregacao = gdf_distritos.astype({'ds_codigo': 'int'}).merge(df_iptu_distrito[df_iptu_distrito.ano == ano].to_pandas_df(), left_on='ds_codigo', right_on='distrito')
-        # gdf_agregacao = gdf_distritos.astype({'ds_codigo': 'int'}).merge(df_iptu_distrito[:].to_pandas_df(), left_on='ds_codigo', right_on='distrito')
+    if agregacao == 'distritos':
+        gdf_agregacao = gdf_distritos.astype({'ds_codigo': 'int'}).merge(df_iptu_distrito[df_iptu_distrito.ano == ano].to_pandas_df(), left_on='ds_codigo', right_on='distrito')[["ds_codigo", "ds_nome", atributo, 'geometry', 'ano', 'Quantidade de Unidades']]
         hover_data = ["ds_nome"]
         custom_data=["ds_codigo"]
         min_max = [df_iptu_distrito[atributo].min().item(), df_iptu_distrito[atributo].max().item()]
 
-    if agregacao == 2:
-        gdf_agregacao = gdf_subprefeitura.astype({'sp_codigo': 'int'}).merge(df_iptu_subprefeitura[df_iptu_subprefeitura.ano == ano].to_pandas_df(), left_on='sp_codigo', right_on='subprefeitura')
+    if agregacao == 'subprefeituras':
+        gdf_agregacao = gdf_subprefeitura.astype({'sp_codigo': 'int'}).merge(df_iptu_subprefeitura[df_iptu_subprefeitura.ano == ano].to_pandas_df(), left_on='sp_codigo', right_on='subprefeitura')[["sp_codigo", "sp_nome", atributo, 'geometry', 'ano',  'Quantidade de Unidades']]
         hover_data = ["sp_nome"]
         custom_data=["sp_codigo"]
         min_max = [df_iptu_subprefeitura[atributo].min().item(), df_iptu_subprefeitura[atributo].max().item()]
 
-    if agregacao == 3:
-        gdf_agregacao = gdf_od.astype({'od_id': 'int'}).merge(df_iptu_od[df_iptu_od.ano == ano].to_pandas_df(), left_on='od_id', right_on='od')        
+    if agregacao == 'zonas-od':
+        gdf_agregacao = gdf_od.astype({'od_id': 'int'}).merge(df_iptu_od[df_iptu_od.ano == ano].to_pandas_df(), left_on='od_id', right_on='od')[["od_id", "od_nome", atributo, 'geometry', 'ano',  'Quantidade de Unidades']]        
         hover_data = ["od_nome"]
         custom_data=["od_id"]
         min_max = [df_iptu_od[atributo].min().item(), df_iptu_od[atributo].max().item()]
 
+
+    # if agregacao == 'quadras':
+    #     gdf_agregacao = gdf_quadras.merge(df_iptu_sq[df_iptu_sq.ano == ano].to_pandas_df(), left_on='sq', right_on='sq')        
+    #     hover_data = ["sq"]
+    #     custom_data=["sq"]
+    #     min_max = [df_iptu_sq[atributo].min().item(), df_iptu_sq[atributo].max().item()]
+
     return gdf_agregacao, hover_data, custom_data, min_max
+
+@app.callback(
+    Output("download-gpkg", "data"),
+    Input("download-button", "n_clicks"),
+    State("checklist-input", "value"),
+    State("range-slider", "value"),
+    State("radioitems-input", "value"),
+    prevent_initial_call=True,
+)
+def func(n_clicks, atributo, ano, agregacao):
+    # return dict(content="Hello world!", filename="hello.txt")
+    return dict(content=sel_agregacao(agregacao, ano[-1], atributo)[0].to_json(), 
+                filename=f"IPTU-SP-{atributo.replace(' ','-')}-{ano[-1]}-{agregacao}.geojson")
+
 
 if __name__ == '__main__':
     # app.run_server(debug=True)
